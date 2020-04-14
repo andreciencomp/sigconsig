c63f124f71d02999043b33913b9748e9e73033cb
Andre Soares <andreciencomp@gmail.com>
2020-04-13 21:37:46 -0300

    deletada a pasta src.

--- src/entidades/Orgao.js
+++
@@ -1,9 +0,0 @@
-class Orgao{
-    constructor(){
-        this.id = 0;
-        this.sigla = "";
-        this.nome = "";
-    }
-}
-
-module.exports = Orgao;
--- src/entidades/Produto.js
+++
@@ -1,9 +0,0 @@
-class Produto{
-
-    constructor(){
-        this.id = 0;
-        this.qtdParcelas = 0;
-        this.carencia = 0;
-        this.orgao = null;
-    }
-}
