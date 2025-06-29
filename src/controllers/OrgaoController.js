f708f90a58a87c7c6d5835fd4a3e341de002350c
andreciencomp <andreciencomp@gmail.com>
2025-06-29 12:25:50 -0300

    Criada a classe OrgaoController e movida a lógica das rotas de orgão para o controlador.

---
+++ src/controllers/OrgaoController.js
@@ -0,0 +1,64 @@
+const ExceptionService = require("../excessoes/ExceptionService");
+const FachadaNegocio = require("../negocio/FachadaNegocio");
+const OrgaoValidator = require("../validators/OrgaoValidator");
+
+class OrgaoController {
+    obterPorId = async (req, res) => {
+        try {
+            const fachada = new FachadaNegocio();
+            const orgao = await fachada.obterOrgaoPorID(req.params.id);
+            return res.status(200).send(orgao);
+
+        } catch (e) {
+            ExceptionService.enviarExcessao(e, res);
+        }
+    }
+
+    cadastrar = async (req, res) => {
+        try {
+            OrgaoValidator.validarCadastro(req.body);
+            const fachada = new FachadaNegocio();
+            const orgaoId = await fachada.cadastrarOrgao(req.body);
+            return res.status(201).send(orgaoId);
+
+        } catch (e) {
+            ExceptionService.enviarExcessao(e, res);
+        }
+    }
+
+    atualizar = async (req, res) => {
+        try {
+            OrgaoValidator.validarAtualizacao(req.body);
+            const fachada = new FachadaNegocio();
+            const orgaoId = await fachada.atualizarOrgao(req.body);
+            return res.status(201).send(orgaoId);
+
+        } catch (e) {
+            ExceptionService.enviarExcessao(e, res);
+        }
+    }
+
+    listar = async (req, res) => {
+        try {
+            const fachada = new FachadaNegocio();
+            const orgaos = await fachada.listarOrgaos();
+            return res.status(200).send(orgaos);
+
+        } catch (e) {
+            ExceptionService.enviarExcessao(e, res);
+        }
+    }
+
+    deletar = async (req, res) => {
+        try {
+            const fachada = new FachadaNegocio();
+            const orgaoId = await fachada.deletarOrgao(req.params.id);
+            return res.status(200).send(orgaoId);
+
+        } catch (e) {
+            ExceptionService.enviarExcessao(e, res);
+        }
+    }
+}
+
+module.exports = OrgaoController
--- src/routes/orgaos-rotas.js
+++ src/routes/orgaos-rotas.js
@@ -1,66 +1,18 @@
 const express = require('express');
-const FachadaNegocio = require('../negocio/FachadaNegocio');
-const ExceptionService = require('../excessoes/ExceptionService');
-const OrgaoValidator = require('../validators/OrgaoValidator');
 const AuthMiddleware = require('../Middleware/AuthMiddleware');
-
+const OrgaoController = require('../controllers/OrgaoController');
 const roteador = express.Router();
 
-roteador.get('/orgaos/:id', AuthMiddleware.nivelAutenticado, async (req, res) => {
-    try {
-        const fachada = new FachadaNegocio();
-        const orgao = await fachada.obterOrgaoPorID(req.params.id);
-        return res.status(200).send(orgao);
+const orgaoController = new OrgaoController();
 
-    } catch (e) {
-        ExceptionService.enviarExcessao(e, res);
-    }
-});
+roteador.get('/orgaos/:id', AuthMiddleware.nivelAutenticado, orgaoController.obterPorId);
 
-roteador.post('/orgaos/cadastrar', AuthMiddleware.nivelAdmin, async (req, res) => {
-    try {
-        OrgaoValidator.validarCadastro(req.body);
-        const fachada = new FachadaNegocio();
-        const orgaoId = await fachada.cadastrarOrgao(req.body);
-        return res.status(201).send(orgaoId);
+roteador.post('/orgaos/cadastrar', AuthMiddleware.nivelAdmin, orgaoController.cadastrar);
 
-    } catch (e) {
-        ExceptionService.enviarExcessao(e, res);
-    }
-});
+roteador.put('/orgaos/atualizar', AuthMiddleware.nivelAdmin, orgaoController.atualizar);
 
-roteador.put('/orgaos/atualizar', AuthMiddleware.nivelAdmin, async (req, res)=>{
-    try{
-        OrgaoValidator.validarAtualizacao(req.body);
-        const fachada = new FachadaNegocio();
-        const orgaoId = await fachada.atualizarOrgao(req.body);
-        return res.status(201).send(orgaoId);
-        
-    }catch(e){
-        ExceptionService.enviarExcessao(e, res);
-    }
-});
+roteador.get('/orgaos', AuthMiddleware.nivelAutenticado, orgaoController.listar);
 
-roteador.get('/orgaos', AuthMiddleware.nivelAutenticado, async (req, res) => {
-    try {
-        const fachada = new FachadaNegocio();
-        const orgaos = await fachada.listarOrgaos();
-        return res.status(200).send(orgaos);
-
-    } catch (e) {
-        ExceptionService.enviarExcessao(e, res);
-    }
-});
-
-roteador.delete('/orgaos/deletar/:id', AuthMiddleware.nivelAdmin, async (req, res) => {
-    try {
-        const fachada = new FachadaNegocio();
-        const orgaoId = await fachada.deletarOrgao(req.params.id);
-        return res.status(200).send(orgaoId);
-
-    } catch (e) {
-        ExceptionService.enviarExcessao(e, res);
-    }
-});
+roteador.delete('/orgaos/deletar/:id', AuthMiddleware.nivelAdmin, orgaoController.deletar);
 
 module.exports = roteador;
